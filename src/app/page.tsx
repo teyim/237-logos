44df9125247eefbea4cb41cf3d59b80a70080ff1
teyim <teyimasobo@gmail.com>
2024-02-04 22:02:06 +0100

    refactor: page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 import Cards from "@/components/sections/Cards";
 import Header from "@/components/sections/Header";
-import { createDropdownOptions } from "@/helpers/category";
 import { promises as fs } from "fs";
 
 export default async function Home() {
-  // company category data
-  const categories = createDropdownOptions();
-
   // load json file
   const companyData = await fs.readFile(
     process.cwd() + "/src/data/logos.json",
